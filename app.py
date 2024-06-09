55e24f0e422afa63470193afcc3c1195942b24b1
lal aravind <slalaravind@gmail.com>
2024-06-09 22:47:41 +0530

    change

--- app.py
+++ app.py
@@ -3,17 +3,15 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-import google.generativeai as genai
-
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain.vectorstores import FAISS
-from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
 load_dotenv()
-genai.configure(api_key=os.getenv("google_api_key"))
+# Assuming you have set your Google API key in the environment variable 'google_api_key'
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.getenv("google_api_key")
 
 # Reading the PDF
 def get_pdf_text(pdf_docs):
